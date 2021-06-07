729d588a49734fe4024a6fafcef38c481c6af72c
mattb0101 <mattbaum666@googlemail.com>
2021-06-07 15:49:11 +0000

    Apache Server

--- example.php
+++ example.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <title>PHP</title>
 </head>
 <body>
     <?php
-        echo "<h1></h1>";
+        echo "<h1>Hello World</h1>";
     ?>
 </body>
 </html>
