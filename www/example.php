6eb48d7f0a0a38c659a16e47989976b5d322ee64
mattb0101 <mattbaum666@googlemail.com>
2021-06-07 16:05:53 +0000

    Index and starting to look at css and php

--- www/example.php
+++ www/example.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="/static/main.css">
     <title>PHP</title>
 </head>
 <body>
     <?php
-        echo "<h1>Hello World!</h1>";
+
+        $count = 3;
+        echo "<h1>$count</h1>";
     ?>
 </body>
 </html>
